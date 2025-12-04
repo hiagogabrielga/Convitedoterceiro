4778e6b4fb62ee0d49892148fe4cea2ac13666c1
Hiago Gabriel <146982525+hiagogabrielga@users.noreply.github.com>
2025-12-04 15:48:43 -0400

    atualizando estrutura

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,109 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import { Microscope, Heart, GraduationCap, Sparkles, Mail } from "lucide-react";
+import React from "react";
+import ModalConvite from "@/components/ModalConvite";
+import ModalZueira from "@/components/ModalZueira";
 export default function Home() {
+  const [showZueria, setShowZueria] = React.useState(false);
+  const [showConvite, setShowConvite] = React.useState(false);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        home
+        <h1>Uma Reação Especial</h1>
+        <div>
+          <h2>Quando elementos especiais se combinam...</h2>
+          <h3>Alunos + Professor → Inesquecível</h3>
+        </div>
+        <p>
+          Algumas reações químicas resultam em compostos estáveis e preciosos. A
+          sua influência em nossas vidas criou uma ligação que queremos
+          preservar para sempre.
+        </p>
+        <div>
+          <h2>A Fórmula do Professor Perfeito</h2>
+          <div>
+            <div>
+              <Microscope />
+            </div>
+            <div>
+              <div>
+                <h3>Conhecimento</h3>
+                <p>
+                  Conhecimento Você nos ensinou a enxergar o mundo através das
+                  lentes da ciência
+                </p>
+              </div>
+              <div>
+                <p>H₂O → Vida</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <GraduationCap />
+            </div>
+            <div>
+              <div>
+                <h3>Dedicação</h3>
+                <p>
+                  Cada aula foi uma experiência única de aprendizado e
+                  descoberta
+                </p>
+              </div>
+              <div>
+                <p>pC₆H₁₂O₆ → Energia</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <Heart />
+            </div>
+            <div>
+              <div>
+                <h3>Inspiração</h3>
+                <p>Sua paixão pela química contagiou todos nós</p>
+              </div>
+              <div>
+                <p>Fe + Você → Determinação</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <Sparkles />
+            </div>
+            <div>
+              <div>
+                <h3>Transformação</h3>
+                <p>
+                  Catalisou nossa transformação de alunos para profissionais
+                </p>
+              </div>
+              <div>
+                <p>Nós + Você → Futuro</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <button onClick={() => setShowZueria(!showZueria)}>pH da Gratidão = ∞</button>
+        </div>
+        <div>
+          <h2>E agora, chegou o momento da pergunta mais importante...</h2>
+          <div>
+            <button onClick={() => setShowConvite(!showConvite)}>
+              <Mail />
+              Abrir convite Especial
+              <Sparkles />
+            </button>
+          </div>
+        </div>
+        {showConvite && <div><ModalConvite/></div>}
+        {showZueria && <div><ModalZueira/></div>}
       </main>
     </div>
   );
---
+++ src/components/ModalConvite/index.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./modalConvite.module.css";
+export default function ModalConvite() {
+  return (
+    <div>
+        <div>
+            <h1>Querido Professor! 🧪</h1>
+            <p>Assim como uma reação exotérmica libera energia e calor, sua presença em nossas vidas liberou conhecimento, inspiração e alegria!</p>
+            <p>Você foi o catalisador perfeito que acelerou nossa jornada de aprendizado, tornando cada conceito complexo em algo fascinante e compreensível.</p>
+            <div>
+                <p>Por isso, com muito carinho e gratidão, gostaríamos de fazer um convite especial:</p>
+                <p>Seja nosso Paraninfo! 🎓✨</p>
+            </div>
+            <p>Queremos que você esteja ao nosso lado neste momento tão</p>
+        </div>
+    </div>
+  )
+}
